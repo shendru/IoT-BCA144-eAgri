091359f1cda47c6ec90c3496afd240b123583d58
Andrew(Shendru) <dev.andrewearl@gmail.com>
2025-05-06 05:19:44 +0800

    Added Inventory UI from Shadcn

--- src/components/inventory/Inventory.tsx
+++ src/components/inventory/Inventory.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { SectionCards } from "@/components/section-cards";
+import { DataTable } from "@/components/data-table";
+
+import data from "../../app/dashboard/data.json";
 
 function Inventory() {
   return (
@@ -7,6 +10,7 @@ function Inventory() {
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
           <SectionCards />
+          <DataTable data={data} />
         </div>
       </div>
     </div>
