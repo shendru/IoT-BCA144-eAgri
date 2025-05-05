02a83c80a1a9ed36909ac374c7f5680c4602b8fe
Andrew(Shendru) <dev.andrewearl@gmail.com>
2025-05-05 23:27:04 +0800

    New Updates Added Dashboard for BCA144

--- src/components/aside/Aside.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Aside() {
-  return <aside className="bg-transparent w-[6rem] h-full">Aside</aside>;
-}
-
-export default Aside;
--- src/components/navbar/NavBar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NavBar() {
-  return <header className="bg-transparent h-[5rem]">NavBar</header>;
-}
-
-export default NavBar;
